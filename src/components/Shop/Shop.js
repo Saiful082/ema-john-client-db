9b7cc2de28e8ad9dbbd68733dfddaeaa6d6f4832
Saiful082 <smsaifulislam082@gmail.com>
2023-07-10 17:22:49 +0600

    fagination added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -40,20 +40,28 @@ const Shop = () => {
         const ids = Object.keys(storedCart);
         console.log(ids);
 
-        fetch('')
+        fetch('http://localhost:5000/productsByIds', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+
+            },
+            body: JSON.stringify(ids)
+        })
         .then(res => res.json())
         .then(data => {
-            
-        })
-        for(const id in storedCart){
-            const addedProduct = products.find(product => product._id === id);
-            if(addedProduct){
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
+            for(const id in storedCart){
+                const addedProduct = data.find(product => product._id === id);
+                if(addedProduct){
+                    const quantity = storedCart[id];
+                    addedProduct.quantity = quantity;
+                    savedCart.push(addedProduct);
+                }
             }
-        }
-        setCart(savedCart);
+            setCart(savedCart);
+        })
+      
+      
     }, [products])
 
     const handleAddToCart = (selectedProduct) =>{
